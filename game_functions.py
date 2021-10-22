14b85e40843354f75df646f8eeb8d9e6c68589f6
Jefferson Yuri <jeffyuri@hotmail.com>
2021-10-22 20:19:19 -0300

    Ajuste na velocidade da espaçonave, para ficar com movimento mais fluido

--- game_functions.py
+++ game_functions.py
@@ -3,6 +3,7 @@ import pygame
 
 
 def check_events(ship):
+    """Responde a eventos de pressionamento de teclas e de mouse"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -10,9 +11,13 @@ def check_events(ship):
             if event .key == pygame.K_RIGHT:
                 #Move a espaçonave para a direita
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
